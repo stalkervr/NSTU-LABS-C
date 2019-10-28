4da3f34d84ffb8c0fd6adfe4a8f79ac0ca0ea9c0
stalkervr <stalkervr@gmail.com>
2019-10-28 10:41:53 +0700

    Добавил функцию поиска чёрных вторников и пятниц

--- Lab_2_v_12/main.c
+++ Lab_2_v_12/main.c
@@ -1,6 +1,6 @@
 #include "header.h"
 
-int findBlackDays();
+int findBlackDays(int currentYear, int* arrayOfMonths, int startDay);
 bool checkLeapYear(int currentYear);
 
 int main() {
@@ -8,14 +8,23 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	printf("Лабораторная работа №2 вариант 12.\n");
 
-	int arrayOfMonths[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }, start, countBlackDays = 0, currentYear;
-	printf("Введите каким днём недели является 1 Января \n (1-понедельник, 2-вторник, 3-среда, 4-четверг, 5-пятница, 6-суббота, 7-воскресенье):");
+	int arrayOfMonths[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	int startDay = 0;
+	int currentYear = 0;
 
-	scanf_s("%d", &start);
+	printf("Введите каким днём недели является 1 Января \n (1-понедельник, 2-вторник, 3-среда, 4-четверг, 5-пятница, 6-суббота, 7-воскресенье):");
+	scanf_s("%d", &startDay);
 
 	printf("Введите год : ");
-
 	scanf_s("%d", &currentYear);
+
+	printf("\nКоличество чёрных вторников и пятниц в этом году составляет : %d \n", findBlackDays(currentYear, arrayOfMonths, startDay));
+	return 0;
+}
+
+int findBlackDays(int currentYear, int* arrayOfMonths, int startDay)
+{
+	int countBlackDays = 0;
 
 	if (checkLeapYear(currentYear)) {
 		arrayOfMonths[1] = 29;
@@ -25,22 +34,17 @@ int main() {
 	{
 		for (int days = 1; days <= arrayOfMonths[i]; days++)
 		{
-			printf("%d|%d ", days, start);
-			if (days == 13 && ((start == 2) || (start == 5)))
+			printf("%d|%d ", days, startDay);
+			if (days == 13 && ((startDay == 2) || (startDay == 5)))
 			{
 				countBlackDays++;
 			}
-			start = (start % 7) + 1;
+			startDay = (startDay % 7) + 1;
 		}
 		printf("\n\n");
 	}
-	printf("\nКоличество чёрных вторников и пятниц в этом году составляет : %d \n", countBlackDays);
-	return 0;
-}
-
-int findBlackDays()
-{
-	return 0;
+	
+	return countBlackDays;
 }
 
 bool checkLeapYear(int currentYear)
