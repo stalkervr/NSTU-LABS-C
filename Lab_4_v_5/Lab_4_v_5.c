03df96cfeeea06bb27f148c7916594d821d01176
stalkervr <stalkervr@gmail.com>
2020-02-13 10:15:46 +0700

    Рабочий вариант №1

--- Lab_4_v_5/Lab_4_v_5.c
+++ Lab_4_v_5/Lab_4_v_5.c
@@ -2,9 +2,14 @@
 
 #include <stdio.h>
 #include <locale.h>
-#include <math.h>
+
+void replaceShort(char str[], char str1[], char str2[]);
+
+int searchShort(char A[], char B[], int pos);
 
 void replaceWrapper(char str[], char str1[], char str2[]);
+
+int abs(int number);
 
 int lengthString(char string[]);
 void leftShift(char A[], int pos, int num);
@@ -19,18 +24,94 @@ int main()
 
 	char searchPattern[5][5] = { "=","==","!=","a+=","a-=" };
 	char changePattern[5][5] = { ":=","=","#","a=a+","a=a-" };
-	char str[300] = "dfg*=*gsg*==*grg**klfgj";
-	//char str1[] = "==";
-	//char str2[] = "*";
-	//int pos = 0;
+	char str[300] = "(=, ==, !=, a+=, a-=, =)";
+
 	printf("Строка до замены => %s\n\n", str);
 
-	//rightShift(str, 4, 1);
+	replaceShort(str, "=", ":=");
 	replaceWrapper(str, "==", "=");
+	replaceWrapper(str, "!=", "#");
+	replaceWrapper(str, "a+=", "a=a+");
+	replaceWrapper(str, "a-=", "a=a-");
+	//printf("Найден символ = его индекс -> %d", searchShort(str,"=",0));
 
 	printf("Строка после замены => %s\n", str);
 
     return 0;
+}
+
+int abs(int number)
+{
+	return number < 1 ? number * (-1) : number;
+}
+
+int searchShort(char A[], char B[], int pos)
+{
+	int i, j;
+	int lengthA, lengthB;
+	lengthA = lengthString(A);
+	lengthB = lengthString(B);
+	// поиск построки "="
+
+	// проверка если длинна искомой строки больше строки для поиска её там точно нет
+	if (lengthB > lengthA)
+	{
+		return -1;
+	}
+
+	for (i = pos; i <= lengthA - lengthB; i++)
+	{
+		for (j = 0; j < lengthB; j++)
+		{
+			if (A[i + j] != B[j])// переход к следующему символу строки В(на след итерацию(j++) только если условие ложно 
+			{
+				break;
+			}
+		}
+		if (j == lengthB)
+		{
+			if ( A[i] == '=' &&
+				 A[i-1] != '=' && 
+				 A[i+1] != '=' && 
+				 A[i-1] != '!' && 
+				 A[i-1] != '+' &&
+				 A[i-1] != '-' &&
+				 A[i-1] != 'a')
+			{
+				return i;
+			}
+		}
+	}
+	
+	return -1;
+}
+
+void replaceShort(char str[], char str1[], char str2[])
+{
+	int pos = 0;
+	int shiftPoint;
+	int deltaShift = lengthString(str1) - lengthString(str2);
+
+	while (pos != -1)
+	{
+		pos = searchShort(str, str1, pos);
+
+		shiftPoint = pos + lengthString(str1);
+
+		if (deltaShift < 0)
+		{
+			rightShift(str, shiftPoint, abs(deltaShift));
+		}
+		if (deltaShift > 0)
+		{
+			leftShift(str, shiftPoint - deltaShift, deltaShift);
+		}
+
+		substringInsertion(str, str2, pos);
+
+		pos += lengthString(str1) + abs(deltaShift);
+		pos = searchShort(str, str1, pos);
+	}
 }
 
 void replaceWrapper(char str[], char str1[], char str2[])
@@ -38,6 +119,7 @@ void replaceWrapper(char str[], char str1[], char str2[])
 	int pos = 0;
 	int shiftPoint;
 	int deltaShift = lengthString(str1) - lengthString(str2);
+
 	while (pos != -1)
 	{
 		pos = substringSearch(str, str1, pos);
@@ -46,7 +128,7 @@ void replaceWrapper(char str[], char str1[], char str2[])
 
 		if (deltaShift < 0)
 		{
-			rightShift(str, shiftPoint , deltaShift*(-1));
+			rightShift(str, shiftPoint , abs(deltaShift));
 		}
 		if (deltaShift > 0)
 		{
@@ -90,7 +172,6 @@ void rightShift(char A[], int pos, int num)
 	{
 		A[i + num] = A[i];
 	}
-	//A[pos] = ' ';
 }
 
 int substringSearch(char A[], char B[], int pos)
@@ -99,12 +180,15 @@ int substringSearch(char A[], char B[], int pos)
 	int lengthA, lengthB;
 	lengthA = lengthString(A);
 	lengthB = lengthString(B);
+	// поиск построки "="
+
 	// проверка если длинна искомой строки больше строки для поиска её там точно нет
 	if (lengthB > lengthA)
 	{
 		return -1;
 	}
 
+	
 	for (i = pos; i <= lengthA - lengthB; i++)
 	{
 		for (j = 0; j < lengthB; j++)
@@ -119,6 +203,7 @@ int substringSearch(char A[], char B[], int pos)
 			return i;
 		}
 	}
+	
 	return -1;
 }
 
