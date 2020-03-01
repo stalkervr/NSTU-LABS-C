4935e31f9c6c28ad09b4c764c2b2a7ef690dca66
stalkervr <stalkervr@gmail.com>
2020-03-01 11:51:09 +0700

    Лабораторная работа № 5

---
+++ Lab_5_v_2/Lab_5_v_2.cpp
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <stdio.h>
+#include <locale.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+void immersionInsertionSorting(int arr[], int n);
+void shellSorting(int arr[], int amountOfElements);
+
+
+int main()
+{
+	srand(time(NULL));
+	return 0;
+}
+
+void immersionInsertionSorting(int arr[], int n) {
+
+	for (int i = 1; i < n; i++)// Пока не достигли " дна" или меньшего себя
+
+		for (int k = i; k != 0 && arr[k] < arr[k - 1]; k--)
+		{
+			int temp = arr[k];
+			arr[k] = arr[k - 1];
+			arr[k - 1] = temp;
+		}
+}
+
+void shellSorting(int arr[], int amountOfElements)
+{
+	int step = amountOfElements / 2;   //инициализируем шаг.
+	while (step > 0)       //пока шаг не 0
+	{
+		for (int i = 0; i < (amountOfElements - step); i++)
+		{
+			int j = i;
+			while (j >= 0 && arr[j] > arr[j + step])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + step];
+				arr[j + step] = temp;
+				j--;
+			}
+		}
+		step = step / 2;
+	}
+}
